29ff9aa310e6f5eaee20eacdb00559deceb5b5ae
lukecheetham <71508800+lukecheetham@users.noreply.github.com>
2020-09-29 13:22:00 +0100

    Add files via upload

---
+++ EulerProblem2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace EulerProblem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fibFirst = 0;
+            int fibNext = 1;
+            int result = 0;
+            int sum = 0;
+
+            while (result < 4000000)
+            {
+                result = fibFirst + fibNext;
+                fibFirst = fibNext;
+                fibNext = result;
+
+                if ((result % 2) == 0)
+                {
+                    sum += result;
+                    Console.WriteLine($"{result}, ");
+                }
+            }
+
+            Console.WriteLine($"Total sum of all the even numbers in the Fibonacci sequence is: {sum}");
+        }
+    }
+}
